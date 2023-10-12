2730c8857dbfb77c5926f02a4b88b7dd8c146cf8
kaique donizeti <kaiquedonizetisilva@gmail.com>
2023-10-11 21:41:49 -0300

    Add files via upload

---
+++ Desafio-44/desafio.py
@@ -0,0 +1,28 @@
+x = 0
+
+for x in range(5):
+
+    print('Hello World!')
+
+print("----------------------")
+
+for c in range(10, 0, -1):
+
+    print(f"{c}")
+
+print("----------------------")
+
+x = 1
+while x <= 150:
+    print(x)
+    x = x + 1
+
+print("----------------------")
+
+for i in range(202):
+
+    if(i%2==0):
+
+        print(i, "Números pares!")
+
+print("----------------------")
